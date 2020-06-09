06b8184bd3d49420c0f021cb40d7ea7570458f75
psy15 <parasy152@gmail.com>
2020-06-09 09:45:58 +0530

    X00T: lineage makeup

--- lineage_X00T.mk
+++ lineage_X00T.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common Havoc stuff
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from X00T device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -28,7 +28,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := X00T
 PRODUCT_MANUFACTURER := asus
-PRODUCT_NAME := havoc_X00T
+PRODUCT_NAME := lineage_X00T
 PRODUCT_MODEL := ZenFone Max Pro M1
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
@@ -42,6 +42,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.021/4565141:user/release-keys
 
-HAVOC_BUILD_TYPE := Official
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.havoc.maintainer=rizkybenggolo
+# HAVOC_BUILD_TYPE := Official
+# PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.havoc.maintainer=rizkybenggolo
